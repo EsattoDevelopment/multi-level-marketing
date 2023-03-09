dd0afd2a2216822fc577869a291b331209a2105a
Gustavo Eklund <gustavo04616@gmail.com>
2023-03-09 15:54:44 -0300

    refactor: explicit final value and invert installments order

--- resources/views/default/emprestimo/calculadora.blade.php
+++ resources/views/default/emprestimo/calculadora.blade.php
@@ -40,13 +40,31 @@
             <div class="center-block text-center">
                 <h2>Simulação para um empréstimo de <b>{{ mascaraMoeda($sistema->moeda, $valor, 2, true) }}</b></h2>
             </div>
-            <ul class="parcelas">
+            <ul class="grupo">
                 @foreach($parcelas as $grupo => $lista_parcelas)
                     <li class="box box-solid">
-                        <h3>{{ $grupo }}</h3>
-                        <ul>
-                            @foreach($lista_parcelas as $parcela)
-                                <li>{{ $parcela['nome'] }} - {{ mascaraMoeda($sistema->moeda, $parcela['valor_total'], 2, true) }}</li>
+                        <div>
+                            <h3>{{ $grupo }}</h3>
+                        </div>
+                        <ul class="parcelas">
+                            @foreach(array_reverse($lista_parcelas) as $key => $parcela)
+                                <li>
+                                    <span>{{ str_pad($parcela['nome'], 3, '0', STR_PAD_LEFT) }} de </span>
+                                    @if($key === 0)
+                                        <span>
+                                            <strong>
+                                                <sup>{{ $sistema->moeda }}</sup>
+                                                <span style="font-size: 32px">{{ mascaraMoeda($sistema->moeda, $parcela['valor_total'], 0) }}</span>
+                                                <sub>{{substr(explode('.', (string) $parcela['valor_total'])[1], 0, 2)}}</sub>
+                                            </strong>
+                                        </span>
+                                        <div class="mais-popular">
+                                            <span>Mais Popular</span>
+                                        </div>
+                                    @else
+                                        <span>{{ mascaraMoeda($sistema->moeda, $parcela['valor_total'], 2, true) }}</span>
+                                    @endif
+                                </li>
                             @endforeach
                         </ul>
                     </li>
@@ -73,31 +91,72 @@
             max-width: 230px;
             outline: none;
         }
-        .parcelas {
+        .grupo {
             display: flex;
             justify-content: center;
             flex-direction: row;
             list-style: none;
             line-height: 2;
         }
-        .parcelas h3 {
+        .grupo h3 {
             margin-top: 0;
         }
-        .parcelas > li {
+        .grupo > li {
             margin: 0 12px;
             padding: 24px;
             width: auto;
             min-width: 300px;
         }
         @media screen and (max-width: 700px) {
-            .parcelas {
+            .grupo {
                 flex-direction: column;
                 margin: 0;
                 padding: 0;
             }
-            .parcelas > li {
+            .grupo > li {
                 margin: 16px 0;
             }
+        }
+        .parcelas {
+            list-style: none;
+            padding-left: 0;
+            overflow: hidden;
+        }
+        .parcelas > li {
+            padding: 0 16px;
+            position: relative;
+        }
+        .parcelas > li:nth-child(odd) {
+            background-color: #efefef;
+        }
+        .parcelas > li:nth-child(1) {
+            background-color: #3c8dbc;
+            color: #fff;
+            font-size: 18px;
+        }
+        .mais-popular {
+            position: absolute;
+            top: 0;
+            right: 0;
+            width: 64px;
+            height: 64px;
+        }
+        .mais-popular > span {
+            width: 120px;
+            height: 16px;
+            font-size: 12px;
+            font-weight: bold;
+            top: 18px;
+            left: -32px;
+            text-transform: uppercase;
+            position: absolute;
+            background-color: #fff;
+            color: #333333;
+            transform: rotate(35deg);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            pointer-events: none;
         }
     </style>
 @endsection
